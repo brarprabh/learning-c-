c1f7b96a660e9b7627067a2aad0bad87ade4979b
brarprabh <prabhjotsinghbrar39@gmail.com>
2025-08-05 10:17:35 +0530

    add rotate elements

--- learningc++/hello.cpp
+++ learningc++/hello.cpp
@@ -337,40 +337,69 @@
 
 // largest and second largest elements
 
+// # include <bits/stdc++.h>
+// using namespace std;
+// vector<int> greatest(vector<int> &a, int n) {
+//     int largest = INT_MIN;
+//     int slargest = INT_MIN;
+//     for(int i = 0; i < n; i++) {
+//         if(a[i] > largest) {
+//             slargest = largest;
+//             largest = a[i];
+//         }
+//         else if(a[i] < largest && a[i] > slargest) slargest = a[i];
+//     }
+//     return {largest, slargest};
+// }
+// vector<int> answer(vector<int> &a, int n)
+// {
+//   return  greatest(a, n);
+// }
+// int main() {
+//     int n;
+//     cin>>n;
+
+//     // int arr[n];
+//     // for(int i = 0; i < n; i++){
+//     //     cin >> arr[i];
+//     //}
+//     vector<int> arr;  // no need to use[]
+// for(int i = 0; i < n; i++) {
+//     int x;
+//     cin >> x;
+//     arr.push_back(x);  // insert will not work
+// }
+
+//   vector<int> ans = answer(arr, n);   //jive structure hunda si
+//   cout << ans[0] << " " << ans[1]; 
+//     return 0;
+// }
+
+
+
+// rotate array by d elements
+
 # include <bits/stdc++.h>
 using namespace std;
-vector<int> greatest(vector<int> &a, int n) {
-    int largest = INT_MIN;
-    int slargest = INT_MIN;
-    for(int i = 0; i < n; i++) {
-        if(a[i] > largest) {
-            slargest = largest;
-            largest = a[i];
-        }
-        else if(a[i] < largest && a[i] > slargest) slargest = a[i];
+void rotate(int arr[], int n, int d) {
+    d = d % n;
+    int temp[d];
+    for(int i = 0; i < d; i++) {
+        temp[i] = arr[i]; 
     }
-    return {largest, slargest};
-}
-vector<int> answer(vector<int> &a, int n)
-{
-  return  greatest(a, n);
+    for(int i = d; i < n; i++) {
+        arr[i-d] = arr[i];
+    }
+    for(int i = n-d; i < n; i++) { // kyunki more than d h skde aa
+        arr[i] = temp[i - (n-d)];
+    }
 }
 int main() {
-    int n;
-    cin>>n;
+    int n; cin >> n;
+    int d; cin >> d;
+    int arr[n] ;
+    for(int i = 0; i < n; i++) cin >> arr[i];
+    rotate(arr, n, d);
+    for(int i = 0; i < n; i++) cout << arr[i];
 
-    // int arr[n];
-    // for(int i = 0; i < n; i++){
-    //     cin >> arr[i];
-    //}
-    vector<int> arr;
-for(int i = 0; i < n; i++) {
-    int x;
-    cin >> x;
-    arr.push_back(x);
-}
-
-  vector<int> ans = answer(arr, n);
-  cout << ans[0] << " " << ans[1];
-    return 0;
 }
